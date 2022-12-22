3a3f55a618680aa3c922df3f67c85587ec174676
sammo419-tech <smontes2022@gmail.com>
2022-12-22 03:12:04 -0500

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+print("Welcome to Madlibs, a word game that makes you laugh \
+    \n========")
+
+one = input("Please enter a noun: ")
+two = input("Please enter a plural noun: ")
+three = input("Please enter a verb (present tense): ")
+four = input("Please enter a verb (present tense): ")
+five = input("Please enter a part of the body (plural): ")
+six = input("Please enter a adjective: ")
+seven = input("Please enter a plural noun: ")
+eight = input("Please enter a adjective: ")
+
+print("===========")
+
+print(f"Today, every student has a computer small enough to fit into his {one}.\
+    \nHe can solve any math problem by simply pushing the computer's little {two}. \
+    \nComputers can add, multiply, divide, and {three}. They can also {four} \
+    \nbetter than a human. Some computers are {five}. Others have a/an \
+    \n{six} screen that shows all kinds of {seven} and {eight} figures. ")
